4ebca7f77983e9398190b722fdab123d6569473e
sujeetkumar2007 <sujeet.tssk@gmail.com>
2026-02-14 19:16:05 +0530

    2nd commit of the day

---
+++ PrintDemo.java
@@ -0,0 +1,6 @@
+public class PrintDemo{
+public static void main(String[] args){
+System.out.println("HEllo world");
+int age = 18;
+System.out.println("My age is "+age);
+}}
---
+++ VariableDemo.java
@@ -0,0 +1,10 @@
+public class VariableDemo{
+    public static void main(String[] args){
+            int age = 18;
+            double price =99.50;
+            char grade = 'A';
+            boolean isPassed = true;
+            String name = "Sujeet";
+            System.out.println("name: " +name);
+    }
+}
