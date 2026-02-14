6d431627d5b725fdd12258a60e2b0f9965211f57
sujeetkumar2007 <sujeet.tssk@gmail.com>
2026-02-14 19:39:44 +0530

    Starting with java done

---
+++ StudentInfo.java
@@ -0,0 +1,14 @@
+public class StudentInfo{
+public static void main(String[] args){
+    String name = "Sujeet";
+    int year = 1;
+    String branch = "CSE";
+    double cgpa = 9.91;
+    boolean isHosteller = false;
+    System.out.println("====Info====");
+    System.out.println("Name: " + name);
+    System.out.println("Year: " + year);    
+    System.out.println("Branch: " + branch);
+    System.out.println("CGPA: " + cgpa);
+    System.out.println("Hosteller: " + isHosteller);
+}}
